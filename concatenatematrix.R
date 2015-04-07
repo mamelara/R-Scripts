8775a8632bf7dbf2e337be739919137c0617b665
Mario Melara <mariomelara@Marios-MacBook-Pro.local>
2015-04-06 17:26:15 -0700

    Fixed test for median

--- concatenatematrix.R
+++ concatenatematrix.R
@@ -30,7 +30,7 @@ expression_matrix = expression_matrix[complete.cases(expression_matrix),]
 #If the median of the averages is less than 100 he throws the entire thing out.
 expression_matrix = cbind(expression_matrix,rowMeans(expression_matrix))
 
-if(median(mat[,ncol(mat)])) < 100 {
+if(median(mat[,ncol(mat)]) < 100) {
    rm(mat) }
 else{
     return mat}
