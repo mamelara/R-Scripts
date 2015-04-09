fca50f5c7aa976d80f5bb667934692617de87bbb
Mario Melara <mariomelara@marios-mbp.attlocal.net>
2015-04-08 22:17:16 -0700

    Find method to loop through each dataset and grab their means

--- concatenatematrix.R
+++ concatenatematrix.R
@@ -10,14 +10,17 @@ myFiles = list.files(dirpath,pattern="*.txt",full.name=T)
 GEO_list = lapply(myFiles,function(x){
     getGEO(filename=as.character(x),GSEMatrix=TRUE)})
 
-#Create an expression matrix with the first entry of the list
-expression_matrix = exprs(GEO_list[[1]])
+#Create an empty matrix to start appending to?
+
+#Then loop through the remaining entries of the list, first check the rowMeans of the entry. If it's less than 100 delete it.
+#Take row means of every dataset and then check the maximum value. Then you want to get rid of expressions < 200 of each row
+for(i in 1:length(GEO_list)) {
+  if(max(rowMeans(exprs(GEO_list[[i]]))) < 100){
+      GEO_list[[i]] = NULL
+  }
+  
 
 
-#Then loop through the remaining entries of the list
-for(i in 2:length(GEO_list)) {
-  expression_matrix = combine(expression_matrix,exprs(GEO_list[[i]]))
-}
 #Get rid of NA's
 expression_matrix = expression_matrix[complete.cases(expression_matrix),]
 
@@ -30,9 +33,9 @@ expression_matrix = expression_matrix[complete.cases(expression_matrix),]
 #If the median of the averages is less than 100 he throws the entire thing out.
 gene_expression_means = rowMeans(expression_matrix)
 
-if(median(gene_expression_means) < 100) {
-   rm(expression_matrix) }
-else{
-    return(expression_matrix)
-}
+#if(median(gene_expression_means) < 100) {
+#   rm(expression_matrix) }
+#else{
+#    return(expression_matrix)
+#}
 }
